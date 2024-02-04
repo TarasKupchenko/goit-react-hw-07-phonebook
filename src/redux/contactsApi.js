f7eb2630355c73b0c532bc3b1a60083aae97d68a
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-04 16:14:42 +0200

    start

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'https://65bf6fc125a83926ab94f86b.mockapi.io';
+const url = 'https://65bf6fc125a83926ab94f86b.mockapi.io/contacts';
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkApi) => {
     try {
-      const response = await axios.get(`${url}/contacts`);
+      const response = await axios.get(`${url}`);
       return response.data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
@@ -18,7 +18,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkApi) => {
     try {
-      const response = await axios.post(`${url}/contacts`, contact);
+      const response = await axios.post(`${url}`, contact);
       return response.data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
@@ -29,7 +29,7 @@ export const addContact = createAsyncThunk(
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async contactId => {
-    await axios.delete(`${url}/contacts/${contactId}`);
+    await axios.delete(`${url}/${contactId}`);
     return contactId;
   }
 );
